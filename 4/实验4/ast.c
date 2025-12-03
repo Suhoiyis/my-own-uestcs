f631d35a357a9a6fd199e8724c6777b90f370c9f
yu.ubuntu <3218818428@qq.com>
2025-12-03 15:30:38 +0800

    ready to upload

---
+++ 4/实验4/ast.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "ast.h"
+
+void show_ast(ast* node, int nest);
+void print_support(char* type, char* string, int* nest);
+ast* new_node();
+ast* new_dec(int value);
+ast* new_oct(int value);
+ast* new_hex(int value);
+ast* new_expr(int op, ast* left, ast* right);
+ast* new_expr_mul(int op, ast* left, ast* right);
+ast* new_id(char* value);
+ast* new_subscript(ast* next, ast* right);
+ast* new_id_with_subscript(char* value, ast* right);
+ast* new_unary_op(int value);
+ast* new_exp_with_unary_op(ast* left, ast* right);
+ast* new_func_params(ast* next, ast* right);
+ast* new_square_brackets(ast* next, ast* right);
+ast* new_func_param(char* value1, char* value2, ast* left);
+ast* new_func_call_id(char* value, ast* right);
+ast* new_func_call_param(ast* next, ast* right);
+ast* new_const_init(ast* next, ast* right, char* indicator);
+ast* new_const_def_a(ast* next, ast* right);
+ast* new_const_def(char* value, ast* left, ast* right);
+ast* new_const_decl(char* value, ast* right, ast* next);
+ast* new_var_decl(char* value, ast* next, ast* right);
+ast* new_var_def_a(ast* next, ast* right);
+ast* new_var_def(char* value, ast* left, ast* right);
+ast* new_init_var(ast* right, char* indicator, ast* next);
+ast* new_func_def(char* value1, char* value2, ast* left, ast* right);
+ast* new_block_item(ast* next, ast* right);
+ast* new_block(ast* next, ast* right);
+ast* new_stmt(char* value1, char* value2, ast* left, ast* right, ast* extend);
+ast* new_l_or_exp(char* value, ast* left, ast* right);
+ast* new_l_and_exp(char* value, ast* left, ast* right);
+ast* new_eq_exp(char* value, ast* left, ast* right);
+ast* new_rel_exp(char* value, ast* left, ast* right);
+ast* new_comp_unit(ast* next, ast* right);
